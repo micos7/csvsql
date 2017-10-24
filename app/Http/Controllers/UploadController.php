19e3a770e283ffb8f05ee9fef3bd5d820917445c
micos7 <micos7@gmail.com>
2017-10-24 21:36:53 +0300

    Upload Controller

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,27 @@
+<?php 
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use Input;
+use Validator;
+use Redirect;
+use View;
+
+class UploadController extends Controller {
+
+	public function view() {
+		return view('imageUpload');
+	}
+
+	public function upload(Request $request) {
+		$this->validate($request, [
+	    	'image' => 'mimes:jpeg,bmp,png', //only allow this type extension file.
+		]);
+
+		$file = $request->file('image');
+		// image upload in public/upload folder.
+		$file->move('uploads', $file->getClientOriginalName()); 
+		echo 'Image Uploaded Successfully';
+	}
+}

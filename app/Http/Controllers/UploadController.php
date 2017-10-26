a08c2590a4cc13e70f94e0b563806bdba50e9e5f
micos7 <micos7@gmail.com>
2017-10-26 22:29:48 +0300

    new extension

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -16,7 +16,7 @@ class UploadController extends Controller {
 
 	public function upload(Request $request) {
 		$this->validate($request, [
-	    	'image' => 'mimes:jpeg,bmp,png', //only allow this type extension file.
+	    	'image' => 'mimes:jpeg,bmp,jpg,png', //only allow this type extension file.
 		]);
 
 		$file = $request->file('image');

6856bac8102d3f6c8ce132330a2e3194018d540a
unknown <micos7@gmail.com>
2017-11-25 13:41:40 +0200

    Some styling

--- resources/views/imageUpload.blade.php
+++ resources/views/imageUpload.blade.php
@@ -9,15 +9,17 @@
 		<div class="content">
 			<h1>File Upload</h1>
 			<form action="{{ URL::to('upload') }}" method="post" enctype="multipart/form-data">
-				<label>Select image to upload:</label>
-				<input type="hidden" value="{{ csrf_token() }}" name="_token">
-				<input type="file" name="image" id="file">
+				<div class="form-group">
+					<label>Select image to upload:</label>
+					<input type="hidden" value="{{ csrf_token() }}" name="_token">
+					<input type="file" name="image" id="file">
+				</div>
 				@if ($errors->has('image'))
 	            	<span class="help-block">
 	                	<strong>{{ $errors->first('image') }}</strong>
 	            	</span>
 	        	@endif
-				<input type="submit" value="Upload" name="submit">
+				<input type="submit" value="Upload" class="btn btn-default" name="submit">
 			</form>
 		</div>
 	</div>

074170f350bd3b271aa5154b47c133b59a5cf2ac
micos7 <micos7@gmail.com>
2018-03-18 17:16:06 +0200

    Bootstrap 4

--- resources/views/imageUpload.blade.php
+++ resources/views/imageUpload.blade.php
@@ -2,7 +2,7 @@
 <head>
 	<title>CSV to SQL</title>
 	<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
 	<div class="container">

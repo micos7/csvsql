382efbc8bf928730591ddcb84da0cc1e04299d61
micos7 <micos7@gmail.com>
2018-08-11 20:31:36 +0300

    home

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/upload', function () {
+Route::get('/', function () {
     return view('imageUpload');
 });
 
